cdfdba0baaaac25c975b229053f02ba522157976
Art deHoyos <adehoyos@gmail.com>
2017-03-11 20:35:59 -0600

    Initial commit. Just messages for now.

---
+++ message-types.js
@@ -0,0 +1,17 @@
+module.exports = {
+    server: {
+        WELCOME: 1,
+        UPDATE: 2,
+        NEWPLAYER: 3,
+        PLAYERLEFT: 4,
+        ROUNDBEGIN: 5,
+        ROUNDEND: 6
+    },
+    client: {
+        HELLO: 1,
+        GOODBYE: 2,
+        INPUT: 3,
+        PLAYERREADY: 4,
+        SHIPCONFIG: 5
+    }
+}
