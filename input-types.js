7effcf78f1ca4c17a6e4efff74507ea7d6fa7e4b
Art deHoyos <adehoyos@gmail.com>
2017-03-19 19:44:02 -0500

    added input-types

---
+++ input-types.js
@@ -0,0 +1,16 @@
+module.exports = {
+    UP: 1,
+    DOWN: 2,
+    LEFT: 3,
+    RIGHT: 4,
+    BUTTON_1: 5,
+    BUTTON_2: 6,
+    BUTTON_3: 7,
+    BUTTON_4: 8,
+    BUTTON_5: 9,
+    BUTTON_6: 10,
+    BUTTON_7: 11,
+    BUTTON_8: 12,
+    BUTTON_9: 13,
+    BUTTON_10: 14
+}
